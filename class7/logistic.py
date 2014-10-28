f7b835d02fb2fb810458203fc66e773db1d55b9a
Mike Schachter <mike.schachter@gmail.com>
2014-10-28 12:48:55 -0700

    more code for the logistic regression class

--- class7/logistic.py
+++ class7/logistic.py
@@ -7,6 +7,8 @@ from sklearn.cross_validation import KFold
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+
 
 def load_binary_digits(noise_std=5.00):
     """
@@ -56,6 +58,13 @@ def load_binary_digits(noise_std=5.00):
     return X,y
 
 
+def plot_sigmoid():
+    x = np.linspace(-7, 7, 100)
+    plt.axhline(0.5, color='b')
+    plt.axvline(0.0, color='r')
+    plt.plot(x, 1.0 / (1.0 + np.exp(-x)), 'k-', linewidth=2.0)
+    plt.axis('tight')
+
 
 def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
     """
@@ -63,13 +72,14 @@ def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
     """
 
     best_C = None
-    best_auc = 0
+    best_pc = 0
     best_weights = None
     best_intercept = None
     
     for C in C_to_try:
 
         aucs = list()
+        pcs = list()
         weights = list()
         intercepts = list()
         cmats = list()
@@ -92,6 +102,9 @@ def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
             #normalize each row of the confusion matrix so they represent probabilities
             cmat = (cmat.T / cmat.sum(axis=1)).T
 
+            #compute the percent correct
+            pcs.append((cmat[0, 0] + cmat[1, 1]) / 2.0)
+
             #record the confusion matrix for this fold
             cmats.append(cmat)
 
@@ -110,9 +123,11 @@ def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
         cmats = np.array(cmats)
         Cmean = cmats.mean(axis=0)
 
-        #compute the mean AUC
+        #compute the mean AUC and PC
         mean_auc = np.mean(auc)
         std_auc = np.std(auc, ddof=1)
+        mean_pc = np.mean(pcs)
+        std_pc = np.std(pcs, ddof=1)
 
         #compute the mean weights
         weights = np.array(weights)
@@ -120,13 +135,14 @@ def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
         mean_intercept = np.mean(intercepts)
 
         print 'C={:.4f}'.format(C)
+        print '\tPercent Correct: {:.3f} +/- {:.3f}'.format(mean_pc, std_pc)
         print '\tAUC: {:.3f} +/- {:.3f}'.format(mean_auc, std_auc)
         print '\tConfusion Matrix:'
         print Cmean
 
         #determine if we've found the best model thus far
-        if mean_auc > best_auc:
-            best_auc = mean_auc
+        if mean_pc > best_pc:
+            best_pc = mean_pc
             best_C = C
             best_weights = mean_weights
             best_intercept = mean_intercept
@@ -139,13 +155,89 @@ def fit_binary_data(X, y, C_to_try=[1e-3, 1e-2, 1e-1, 1.0], nfolds=10):
     plt.figure()
     plt.imshow(weights_img, interpolation='nearest', aspect='auto', cmap=cm.seismic, vmin=-weights_absmax, vmax=weights_absmax)
     plt.colorbar()
-    plt.title('Model Weights C: {:.4f}, AUC: {:.2f}, Intercept: {:.3f}'.format(best_C, best_auc, best_intercept))
+    plt.title('Model Weights C: {:.4f}, PC: {:.2f}, Intercept: {:.3f}'.format(best_C, best_pc, best_intercept))
+
+
+def generate_nonlinear_data(plot=False):
+
+    #the decision function is just some wacky arbitrary nonlinear function
+    decision_function = lambda xp,yp: yp**3*np.cos(xp) + xp**3*np.sin(yp)
+
+    npts = 25
+    xvals = np.linspace(-2, 2, npts)
+    yvals = np.linspace(-2, 2, npts)
+    Xcoords,Ycoords = np.meshgrid(xvals, yvals)
+
+    D = decision_function(Xcoords, Ycoords)
+
+    #construct feature matrix and target vector from data
+    X = list()
+    y = list()
+    for xval,yval,dval in zip(Xcoords.ravel(), Ycoords.ravel(), D.ravel()):
+        #decide class based on sign of decision function
+        clz = int(dval) > 0
+        X.append( (xval, yval))
+        y.append(clz)
+    X = np.array(X)
+    y = np.array(y)
+
+    if plot:
+        plt.figure()
+        zi = y == 0
+        plt.plot(X[zi, 0], X[zi, 1], 'ro')
+        plt.plot(X[~zi, 0], X[~zi, 1], 'k^')
+        plt.legend(['0', '1'])
+        plt.axis('tight')
+
+    return X,y
+
+
+def plot_2d_predictions(classifier, X, y):
+
+    #construct a 2D grid of points and then turn it into a feature matrix called Xgrid
+    npts = 25
+    xvals = np.linspace(-2, 2, npts)
+    yvals = np.linspace(-2, 2, npts)
+    Xcoords,Ycoords = np.meshgrid(xvals, yvals)
+    Xgrid = np.array(zip(Xcoords.ravel(), Ycoords.ravel()))
+
+    #compute the predictions for each location in the 2D grid
+    ypred = classifier.predict(Xgrid)
+
+    #plot the actual data
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    zi = y == 0
+    plt.plot(X[zi, 0], X[zi, 1], 'ro')
+    plt.plot(X[~zi, 0], X[~zi, 1], 'k^')
+    plt.legend(['0', '1'])
+    plt.axis('tight')
+    plt.title('Actual Data')
+
+    #plot the predictions
+    plt.subplot(2, 1, 2)
+    zi = ypred == 0
+    plt.plot(Xgrid[zi, 0], Xgrid[zi, 1], 'ro')
+    plt.plot(Xgrid[~zi, 0], Xgrid[~zi, 1], 'k^')
+    plt.legend(['0', '1'])
+    plt.title('Predictions')
+    plt.suptitle(classifier.__class__.__name__)
 
 
 if __name__ == '__main__':
 
-    X,y = load_binary_digits(noise_std=5.0)
-    fit_binary_data(X, y)
+    #X,y = load_binary_digits(noise_std=5.0)
+    #fit_binary_data(X, y)
+    #plot_sigmoid()
+
+    X,y = generate_nonlinear_data()
+    lc = LogisticRegression(C=1e-3)
+    lc.fit(X, y)
+    plot_2d_predictions(lc, X, y)
+
+    svmc = SVC()
+    svmc.fit(X, y)
+    plot_2d_predictions(svmc, X, y)
 
     plt.show()
     
