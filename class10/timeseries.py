d120e2acb76734655b5a4dc48b4cb36620b8dad8
Mike Schachter <mike.schachter@gmail.com>
2014-11-06 10:48:11 -0800

    code for classs 10

--- class10/timeseries.py
+++ class10/timeseries.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import Ridge
 
 
 def generate_sine_wave(freqs=[1.0, 2.0, 5.0], duration=5.0, sample_rate=1e3, plot=True):
@@ -48,11 +49,224 @@ def generate_ar_process(weights=[-0.3, 0.5], duration=0.050, sample_rate=1e3, pl
 
     return t,y
 
+
+def run(transition_function, initial_value, nsteps=100):
+    """ Simulate a system using a difference equation.
+
+        transition_function: The right hand side of the difference equation.
+        initial_value: The starting value for the state.
+        nsteps: The number of steps to run the system for.
+    """
+
+    x = np.zeros([nsteps])
+    x[0] = initial_value
+    for k in range(1, nsteps):
+        x[k] = transition_function(x[k-1])
+
+    return x
+
+
+def difference_equation_examples(logistic_map_r=3.86):
+    """ Show some examples of difference equations.
+
+        logistic_map_r: The value of r to give the logistic
+            map in the third plot. Defaults to 3.86, which
+            creates chaotic dynamics.
+    """
+
+    plt.figure()
+
+    plt.subplot(3, 1, 1)
+    x = run(np.cos, initial_value=1.0, nsteps=20)
+    plt.plot(x, 'k-')
+    plt.axis('tight')
+    plt.title('$x_{t+1} = cos(x_t)$')
+
+    plt.subplot(3, 1, 2)
+    r = 3.57
+    logistic_map = lambda x: r*x*(1.0 - x)
+    x = run(logistic_map, initial_value=0.5, nsteps=75)
+    plt.plot(x, 'k-')
+    plt.axis('tight')
+    plt.title('$x_{t+1} = %0.6f x_t (1 - x_t)$' % r)
+
+    plt.subplot(3, 1, 3)
+    r = logistic_map_r
+    logistic_map = lambda x: r*x*(1.0 - x)
+    x = run(logistic_map, initial_value=0.5, nsteps=175)
+    plt.plot(x, 'k-')
+    plt.axis('tight')
+    plt.title('$x_{t+1} = %0.6f x_t (1 - x_t)$' % r)
+
+
+def plot_power_spectrum(x, sample_rate=1.0):
+
+    #take the fourier transform of the time series x
+    xft = np.fft.fft(x)
+    freq = np.fft.fftfreq(len(x), d=1.0/sample_rate)
+    findex = freq > 0.0
+
+    #square the magnitude of the fourier transform to get
+    #the power spectrum
+    ps = np.abs(xft)**2
+
+    #make a plot
+    plt.figure()
+    plt.plot(freq[findex], ps[findex], 'g-')
+    plt.ylabel('Power')
+    plt.xlabel('Frequency')
+
+
+def autocorrelation_function(y, lags=range(20)):
+    """ Compute the autocorrelation function for the time series y
+        at the given lags.
+    """
+
+    acf = np.zeros([len(lags)])
+    for k,lag in enumerate(lags):
+        #compute the correlation coefficient between y and lagged y
+        C = np.corrcoef(y[:len(y)-lag], y[lag:])
+        acf[k] = C[0, 1]
+
+    return acf,lags
+
+
+def get_system_data(nsteps=500):
+    """ Generate data from linearly filtered smoothed Gaussian noise input. """
+
+    #generate random noise input
+    input = np.random.randn(nsteps)
+
+    #smooth the noise with a hanning window
+    h = np.hanning(30)
+    input_smooth = np.convolve(input, h, mode='same')
+
+    #normalize the input so it's between -1 and 1
+    input_smooth /= np.abs(input_smooth).max()
+
+    ##generate the output by convolving with some sort of oscillating filter
+    the_filter = [1.0, 0.5, 0.0, -0.5, -1.0, -0.25, 0.0, 0.25, 0.0, -0.05, 0.0, 0.05]
+    intercept = 0.7
+    y = np.convolve(input_smooth, the_filter, mode='same') + intercept
+
+    return input_smooth,y,the_filter,intercept
+
+
+def fit_linear_filter(input, output, nlags):
+    """ Fit the weights of a linear filter with nlags between
+        the input time series and the output time series,
+    """
+
+    #generate data matrix
+    X = list()
+    for k in range(nlags, len(output)):
+        X.append(input[k-nlags:k])
+    X = np.array(X)
+
+    #generate target vector
+    y = output[nlags:]
+
+    #do a ridge regression
+    rr = Ridge(alpha=1)
+    rr.fit(X, y)
+
+    #return the filter weights and the bias
+    return rr.coef_[::-1],rr.intercept_
+
+
 if __name__ == '__main__':
 
-    #generate_sine_wave()
-    generate_ar_process()
+    #t,y = generate_sine_wave()
+    #t,y = generate_ar_process(sample_rate=5e3)
+    #difference_equation_examples()
+
+    #sr = 1e3
+    #t,y = generate_sine_wave(freqs=[2.0, 57.0, 143.0], duration=5.0, sample_rate=1e3)
+    #plot_power_spectrum(y, sample_rate=1e3)
+
+    """
+    plt.figure()
+    #plot the ACF of random noise
+    y = np.random.randn(1000)
+    plt.subplot(2, 2, 1)
+    acf,lags = autocorrelation_function(y, lags=range(50))
+    plt.plot(lags, acf, 'k-', linewidth=2.0)
+    plt.title('ACF of Random Noise')
+    plt.axis('tight')
+
+    #plot the ACF of a sine wave
+    t,y = generate_sine_wave(freqs=[2.0, 57.0, 143.0], duration=5.0, sample_rate=1e3, plot=False)
+    plt.subplot(2, 2, 2)
+    acf,lags = autocorrelation_function(y, lags=range(50))
+    plt.plot(lags, acf, 'k-', linewidth=2.0)
+    plt.title('ACF of Sum of Sine Wave')
+    plt.axis('tight')
+
+    #plot the ACF of the logistic map
+    logistic_map = lambda x: 3.86*x*(1.0 - x)
+    y = run(logistic_map, initial_value=0.5, nsteps=500)
+    acf,lags = autocorrelation_function(y, lags=range(100))
+    plt.subplot(2, 2, 3)
+    plt.plot(lags, acf, 'k-', linewidth=2.0)
+    plt.title('ACF of Logistic Map (r=3.86)')
+    plt.axis('tight')
+
+    #plot the ACF of another logistic map
+    logistic_map = lambda x: 3.7*x*(1.0 - x)
+    y = run(logistic_map, initial_value=0.5, nsteps=500)
+    acf,lags = autocorrelation_function(y, lags=range(100))
+    plt.subplot(2, 2, 4)
+    plt.plot(lags, acf, 'k-', linewidth=2.0)
+    plt.title('ACF of Logistic Map (r=3.7)')
+    plt.axis('tight')
+    """
+
+    #r = 3.86
+    #logistic_map = lambda x: r*x*(1.0 - x)
+    #y = run(logistic_map, initial_value=0.5, nsteps=2000)
+    #plot_power_spectrum(y, sample_rate=1.0)
+
+
+    #generate the input/output data
+    input,output,the_filter,the_intercept = get_system_data()
+
+    #fit a linear filter to the input/output data
+    pred_filter,pred_intercept = fit_linear_filter(input, output, 12)
+
+    #generate a predicted output from the input using a convolution
+    pred_output = np.convolve(input, pred_filter, mode='same') + pred_intercept
+
+    #compute the correlation coefficient between the predicted and actual output
+    C = np.corrcoef(output, pred_output)
+    cc = C[0, 1]
+
+    plt.figure()
+    plt.subplot(2, 2, 1)
+    plt.plot(input, 'k-', linewidth=2.0)
+    plt.plot(output, 'r-', linewidth=2.0)
+    plt.xlabel('Time')
+    plt.legend(['Input', 'Output'])
+    plt.axis('tight')
+
+    plt.subplot(2, 2, 2)
+    plt.plot(the_filter, 'bo-', linewidth=2.0)
+    plt.plot(pred_filter, 'co-', linewidth=2.0)
+    plt.xlabel('Lag')
+    plt.legend(['Actual', 'Predicted'])
+    plt.title('Filters')
+    plt.axis('tight')
+
+    plt.subplot(2, 2, 3)
+    plt.plot(output, 'r-', linewidth=2.0)
+    plt.plot(pred_output, 'g-', linewidth=2.0)
+    plt.legend(['Actual Output', 'Predicted Output'])
+    plt.xlabel('Time')
+    plt.axis('tight')
+    plt.title('cc=%0.2f' % cc)
+
     plt.show()
+
+
 
 
 
--- class9/clustering.py
+++ class9/clustering.py
@@ -1,7 +1,14 @@
+from matplotlib import cm
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_guassmix(num_samples_per_cluster=100, plot=True):
+from sklearn.cluster import KMeans
+from sklearn.mixture import GMM
+
+import networkx as nx
+
+
+def generate_guassmix(num_samples_per_cluster=100, plot=True, cluster_probs=(0.33, 0.33, 0.33)):
     """ Generate data from a mixture of 2D Gaussians. """
 
     #the centers of each Gaussian distribution
@@ -16,8 +23,9 @@ def generate_guassmix(num_samples_per_cluster=100, plot=True):
     X = list()
     y = list()
     for k,(mean,cov_mat) in enumerate(zip(centers, cmats)):
-        X.extend(np.random.multivariate_normal(mean, cov_mat, size=num_samples_per_cluster))
-        y.extend([k]*num_samples_per_cluster)
+        nsamps = int(num_samples_per_cluster*cluster_probs[k])
+        X.extend(np.random.multivariate_normal(mean, cov_mat, size=nsamps))
+        y.extend([k]*nsamps)
 
     X = np.array(X)
     y = np.array(y)
@@ -27,18 +35,106 @@ def generate_guassmix(num_samples_per_cluster=100, plot=True):
         plt.figure()
         for k in clusters:
             plt.plot(X[y == k, 0], X[y == k, 1], 'o')
+        plt.title('Data From 3 MV Gaussians')
 
     return X,y
 
 
+def run_kmeans(X, num_clusters=3, plot=True):
+
+    km = KMeans(n_clusters=num_clusters)
+    km.fit(X)
+    ypred = km.predict(X)
+
+    if plot:
+        plt.figure()
+        for k in range(num_clusters):
+            plt.plot(X[ypred == k, 0], X[ypred == k, 1], 'o')
+        plt.title('KMeans Result, num_clusters=%d' % num_clusters)
+
+    return ypred
+
+
+def run_gmm(X, num_clusters=3, plot=True):
+
+    gmm = GMM(n_components=num_clusters, covariance_type='full')
+    gmm.fit(X)
+    ypred = gmm.predict(X)
+
+    #print out the information for each fit cluster
+    for k in range(num_clusters):
+        the_mean = gmm.means_[k]
+        the_cov_mat = gmm.covars_[k]
+        the_cluster_prob = gmm.weights_[k]
+        print 'Cluster %d' % k
+        print '\tProbability: %0.2f' % the_cluster_prob
+        print '\tMean: ',the_mean
+        print '\tCovariance:'
+        print the_cov_mat
+
+    if plot:
+        plt.figure()
+        for k in range(num_clusters):
+            plt.plot(X[ypred == k, 0], X[ypred == k, 1], 'o')
+        plt.title('GMM Clustering, num_clusters=%d' % num_clusters)
+
+    return ypred
+
+
+def generate_social_graph(num_nodes=100, num_clusters=4, plot=True):
+
+    g = nx.Graph()
+
+    #create all the nodes first
+    for k in range(num_nodes):
+        #randomly select a cluster
+        c = np.random.randint(num_clusters)
+
+        #construct nodes
+        g.add_node(k, cluster=c)
+
+    #now connect the nodes with undirected edges, nodes
+    #in the same cluster will have a stronger weight
+    #than those outside of their cluster
+    for n1 in range(num_nodes):
+        #get the cluster for node 1
+        c1 = g.node[n1]['cluster']
+
+        for n2 in range(n1):
+            #get the cluster for node 2
+            c2 = g.node[n2]['cluster']
+
+            #determine the edge weight
+            if c1 == c2:
+                w = np.random.rand()*3
+            else:
+                w = np.random.rand()
+                if w < 0.9:
+                    w = 0.0
+            if w > 0:
+                g.add_edge(n1, n2, weight=w)
+
+    if plot:
+        plt.figure()
+        cluster_colors = ['r', 'g', 'b', 'y']
+        node_clrs = [cluster_colors[g.node[n]['cluster']] for n in g.nodes()]
+        pos = nx.spectral_layout(g, scale=100)
+        weights = [g[n1][n2]['weight'] for n1,n2 in g.edges()]
+
+        nx.draw_networkx(g, pos=pos, node_color=node_clrs, edge_cmap=cm.Greys, edge_vmin=0.0, edge_vmax=3.0, edge_color=weights)
+
+    return g
+
+
 if __name__ == '__main__':
 
-    generate_guassmix(num_samples_per_cluster=100)
+    #X,y = generate_guassmix(num_samples_per_cluster=100, plot=True, cluster_probs=[0.15, 0.35, 0.5])
+    #ypred = run_kmeans(X, num_clusters=3)
+    #ypred = run_gmm(X, num_clusters=5)
+    g = generate_social_graph(num_nodes=100, num_clusters=4, plot=True)
+
+
+
     plt.show()
-
-
-
-
-
 
 
